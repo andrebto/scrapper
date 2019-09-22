74420de75c8093fd1cca23ed20e1a7238baa6bb4
Andre Souza Brito <andresouzabrito@MacBook-Air-de-Andre.local>
2019-09-22 00:49:02 -0300

    remove garbage

--- instagram_scraper.py
+++ instagram_scraper.py
@@ -2,7 +2,6 @@ import os
 import time
 import platform
 import math
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
@@ -52,21 +51,13 @@ class Instagram_scrapper():
 
         num_scrolls = math.ceil((limit - len(div_with_imgs))/ IMGS_PER_SCROLL)
         scroll = 0
-        # Get scroll height
         last_height = self.driver.execute_script("return document.body.scrollHeight")     
 
         while True:
-            # Scroll down to bottom
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-            # Wait to load page
             time.sleep(2.5)
-
-            # Calculate new scroll height and compare with last scroll height
             new_height = self.driver.execute_script("return document.body.scrollHeight")
-            bso = BeautifulSoup(self.driver.page_source)
-            print(len(bso.findAll('div',{'class':'_bz0w'})))
-            if new_height == last_height or num_scrolls == scroll:
+            if new_height == last_height or num_scrolls == scroll: # If new_height == last_height, there is not more images to show
                 break
 
             last_height = new_height
